d9fb6c878b0849244f8158451926f10ef6590a67
Martín <martinalalu@gmail.com>
2022-03-04 16:08:39 -0300

    delete comments

--- R/citas_pasaporte.R
+++ R/citas_pasaporte.R
@@ -1,5 +1,3 @@
-#install.packages(c("janitor", "XML", "RCurl", "rlist", "tidyverse", "telegram.bot"))
-
 library(XML)
 library(RCurl)
 library(rlist)
@@ -13,7 +11,7 @@ tables <- readHTMLTable(url)
 
 df <- tables$`NULL`
 
-#Pasaportes
+
 pasaporte <- df %>%
   clean_names() %>% 
   filter(servicio == 'Pasaportesrenovación y primera vez') %>% 
@@ -32,7 +30,6 @@ proxima_pas <- Sys.Date()
 
 texto_pasaporte <- paste("La ultima apertura de citas para pasporte fue hace", ultima_pas, "dias. Y la proxima", tolower(proxima_pas))
 
-#Nacimientos
 nacimientos <- df %>%
   clean_names() %>% 
   filter(servicio == 'Registro Civil-Nacimientos') %>% 
@@ -51,17 +48,13 @@ if (is.na(proxima_nac)) {
 texto_nacimientos <- paste("La ultima apertura de citas para inscripcion de nacimientos fue hace", ultima_nac, "dias. Y la proxima", tolower(proxima_nac))
 
 
-#Citas
 link_citas <- "Dejo el link para chequear por si les pinta: https://www.cgeonline.com.ar/informacion/apertura-de-citas.html"
 
-#Bot
+
 bot <- Bot(token = "1917827697:AAFjdGbFQQV_kPYFvcCO012duyDrGLxfWHY")
 
-#print(bot$getMe())
-#updates <- bot$getUpdates()
-#chat_id <- updates[[1L]]$from_chat_id()
 
-chat_id <- -671579379 ##Del chat grupal de consulado
+chat_id <- -671579379 
 
 
 bot$sendMessage(chat_id = chat_id, 
@@ -73,19 +66,4 @@ bot$sendMessage(chat_id = chat_id,
 bot$sendMessage(chat_id = chat_id, 
                 text = link_citas)
 
-
-
-##MEJORADO
-
-#if (is.na(proxima_pas)) {
-#  bot$sendMessage(chat_id = chat_id, 
-#                  text = paste("La ultima apertura de citas para pasporte fue hace", ultima_pas, "dias. Y la proxima no se sabe aun..."))
-#} else {
-  
-#  bot$sendMessage(chat_id = chat_id, 
-#                  text = paste("ATENCION!!! HAY CITAS DISPONIBLES PARA TRAMITAR TU PASAPORTE!!
-#                               Ingresa a https://www.cgeonline.com.ar/tramites/citas/modificar/modificar-cita-consulado.html y agenda!"),
-#  bot$senPhoto(chat_id = chat_id, 
-#                  photo = "https://www.intermundial.es/blog/wp-content/uploads/2017/07/11-preguntas-que-debes-saber-sobre-el-pasaporte-espa%C3%B1ol.jpg"))
-#}
 
