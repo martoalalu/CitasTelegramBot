45267815540b736a068ab5e2b9a9b5930c584f05
martoalalu <martinalalu@gmail.com>
2022-03-04 17:17:53 -0300

    test chat_id

--- R/citas_pasaporte.R
+++ R/citas_pasaporte.R
@@ -6,8 +6,10 @@ library(janitor)
 library(telegram.bot)
 
 bot <- Bot(token = "1917827697:AAFjdGbFQQV_kPYFvcCO012duyDrGLxfWHY")
+#chat_id <- -671579379 
 
-chat_id <- -503564772
+
+# CHAT_ID <- -503564772
 
 url <- getURL("https://www.cgeonline.com.ar/informacion/apertura-de-citas.html",.encoding = 'UTF-8',.opts = list(ssl.verifypeer = FALSE) )
 
@@ -23,18 +25,18 @@ proxima_pas <- as.Date(as.character(df[df$servicio=='Pasaportesrenovación y pri
 
 if (is.na(proxima_pas)) {
   
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = paste("\xF0\x9F\x98\x94 Todavia no hay nuevas citas disponibles para pasaporte. La ultima fue hace", ultima_pas, "dias"))
   
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = "Dejo el link para chequear por si les pinta \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/informacion/apertura-de-citas.html")
   
 } else {
  
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = paste("\xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 ATENCION! YA HAY FECHA DISPONIBLE PARA TURNOS DE PASAPORTE, para  el", proxima_pas, "\xF0\x9F\x87\xAA\xF0\x9F\x87\xB8"))
   
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = "Ingresa aca para gestionar tu cita \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/tramites/citas/modificar/modificar-cita-consulado.html")
 }
 
@@ -46,17 +48,17 @@ proximo_nacimiento <- as.Date(as.character(df[df$servicio=='Registro Civil-Nacim
 
 if (is.na(proximo_nacimiento)) {
   
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = paste("\xF0\x9F\x98\x94 Todavia no hay nuevas citas disponibles para nacimientos La ultima fue hace", ultimo_nacimiento, "dias"))
   
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = "Dejo el link para chequear por si les pinta \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/informacion/apertura-de-citas.html")
 } else {
   
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = paste("\xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 ATENCION! YA HAY FECHA DISPONIBLE PARA TURNOS DE NACIMIENTOS,  el", proximo_nacimiento))
   
-  bot$sendMessage(chat_id = chat_id, 
+  bot$sendMessage(chat_id = CHAT_ID, 
                   text = paste("Ingresa aca para gestionar tu cita \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/tramites/citas/modificar/modificar-cita-consulado.html"))
 }
 
