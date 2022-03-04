6594052339e685e0068fcbc56f26a68e7a43bc0d
martoalalu <martinalalu@gmail.com>
2022-03-04 17:13:11 -0300

    update script

--- R/citas_pasaporte.R
+++ R/citas_pasaporte.R
@@ -5,65 +5,58 @@ library(tidyverse)
 library(janitor)
 library(telegram.bot)
 
+bot <- Bot(token = "1917827697:AAFjdGbFQQV_kPYFvcCO012duyDrGLxfWHY")
+
+chat_id <- -503564772
+
 url <- getURL("https://www.cgeonline.com.ar/informacion/apertura-de-citas.html",.encoding = 'UTF-8',.opts = list(ssl.verifypeer = FALSE) )
 
 tables <- readHTMLTable(url)
 
 df <- tables$`NULL`
 
+df <- df %>% clean_names()
 
-pasaporte <- df %>%
-  clean_names() %>% 
-  filter(servicio == 'Pasaportesrenovación y primera vez') %>% 
-  select(ultima_apertura, proxima_apertura)
+ultima_pas <- as.character(difftime(Sys.Date(), as.Date(as.character(df[df$servicio=='Pasaportesrenovación y primera vez',][2]),format = "%d/%m/%Y"),units = c("days")))
+proxima_pas <- as.Date(as.character(df[df$servicio=='Pasaportesrenovación y primera vez',][3]),format = "%d/%m/%Y")
 
-
-ultima_pas <- as.character(difftime(Sys.Date(), as.Date(pasaporte$ultima_apertura[1],format = "%d/%m/%Y"),units = c("days")))
-
-proxima_pas <- as.character(difftime(Sys.Date(), as.Date(pasaporte$proxima_apertura[1],format = "%d/%m/%Y"),units = c("days")))
 
 if (is.na(proxima_pas)) {
-proxima_pas <- "No se sabe"
+  
+  bot$sendMessage(chat_id = chat_id, 
+                  text = paste("\xF0\x9F\x98\x94 Todavia no hay nuevas citas disponibles para pasaporte. La ultima fue hace", ultima_pas, "dias"))
+  
+  bot$sendMessage(chat_id = chat_id, 
+                  text = "Dejo el link para chequear por si les pinta \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/informacion/apertura-de-citas.html")
+  
 } else {
-proxima_pas <- Sys.Date()
+ 
+  bot$sendMessage(chat_id = chat_id, 
+                  text = paste("\xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 ATENCION! YA HAY FECHA DISPONIBLE PARA TURNOS DE PASAPORTE, para  el", proxima_pas, "\xF0\x9F\x87\xAA\xF0\x9F\x87\xB8"))
+  
+  bot$sendMessage(chat_id = chat_id, 
+                  text = "Ingresa aca para gestionar tu cita \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/tramites/citas/modificar/modificar-cita-consulado.html")
 }
 
-texto_pasaporte <- paste("La ultima apertura de citas para pasporte fue hace", ultima_pas, "dias. Y la proxima", tolower(proxima_pas))
 
-nacimientos <- df %>%
-  clean_names() %>% 
-  filter(servicio == 'Registro Civil-Nacimientos') %>% 
-  select(ultima_apertura, proxima_apertura)
 
-ultima_nac <- as.character(difftime(Sys.Date(), as.Date(nacimientos$ultima_apertura[1],format = "%d/%m/%Y"),units = c("days")))
+ultimo_nacimiento <- as.character(difftime(Sys.Date(), as.Date(as.character(df[df$servicio=='Registro Civil-Nacimientos',][2]),format = "%d/%m/%Y"),units = c("days")))
+proximo_nacimiento <- as.Date(as.character(df[df$servicio=='Registro Civil-Nacimientos',][3]),format = "%d/%m/%Y")
 
-proxima_nac <- as.character(difftime(Sys.Date(), as.Date(nacimientos$proxima_apertura[1],format = "%d/%m/%Y"),units = c("days")))
 
-if (is.na(proxima_nac)) {
-  proxima_nac <- "No se sabe"
+if (is.na(proximo_nacimiento)) {
+  
+  bot$sendMessage(chat_id = chat_id, 
+                  text = paste("\xF0\x9F\x98\x94 Todavia no hay nuevas citas disponibles para nacimientos La ultima fue hace", ultimo_nacimiento, "dias"))
+  
+  bot$sendMessage(chat_id = chat_id, 
+                  text = "Dejo el link para chequear por si les pinta \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/informacion/apertura-de-citas.html")
 } else {
-  proxima_nac <- Sys.Date()
+  
+  bot$sendMessage(chat_id = chat_id, 
+                  text = paste("\xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 \xF0\x9F\x98\x83 \xF0\x9F\x87\xAA\xF0\x9F\x87\xB8 ATENCION! YA HAY FECHA DISPONIBLE PARA TURNOS DE NACIMIENTOS,  el", proximo_nacimiento))
+  
+  bot$sendMessage(chat_id = chat_id, 
+                  text = paste("Ingresa aca para gestionar tu cita \xF0\x9F\x91\x87: https://www.cgeonline.com.ar/tramites/citas/modificar/modificar-cita-consulado.html"))
 }
-
-texto_nacimientos <- paste("La ultima apertura de citas para inscripcion de nacimientos fue hace", ultima_nac, "dias. Y la proxima", tolower(proxima_nac))
-
-
-link_citas <- "Dejo el link para chequear por si les pinta: https://www.cgeonline.com.ar/informacion/apertura-de-citas.html"
-
-
-bot <- Bot(token = "1917827697:AAFjdGbFQQV_kPYFvcCO012duyDrGLxfWHY")
-
-
-chat_id <- -671579379 
-
-
-bot$sendMessage(chat_id = chat_id, 
-                text = texto_pasaporte)
-
-bot$sendMessage(chat_id = chat_id, 
-                text = texto_nacimientos)
-
-bot$sendMessage(chat_id = chat_id, 
-                text = link_citas)
-
 
